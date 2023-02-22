ea83f6309702aa459e9f693423df80bdc46c8c16
zhezaaaaaaa <126034467+zhezamuhammadiqbal@users.noreply.github.com>
2023-02-22 09:15:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using v21;
+
+Kalkulator hitung = new Kalkulator();
+
+//hitung.tambah();
+//hitung.kurang();
+
+Console.Write("masukkan panjang : ");
+int p = Convert.ToInt32(Console.ReadLine());
+Console.Write("masukkan lebar : ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+Rumus rumus = new Rumus();
+int luas = rumus.persegiPanjang(p,l);
+
+Console.WriteLine($"luas persegi panjang dengan P = {p} dan L= {l} adalah {luas}");
+
+
---
+++ v21.cs
@@ -0,0 +1,27 @@
+ namespace v21
+{
+  
+   class Kalkulator
+  {
+     public void tambah(){
+        Console.WriteLine("tambah");
+     }
+  
+     public void kurang (){
+        Console.WriteLine("kurang");
+    }
+  }
+
+  class Rumus
+  {
+    public void lingkaran(){
+        Console.WriteLine("lingkaran");
+    }
+
+    public int persegiPanjang(int p, int l){
+      return p*l;
+    }
+  }
+
+
+}
